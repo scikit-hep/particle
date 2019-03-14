73796457a5deb6015b8178f082aab61e002f9116
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-03-14 11:46:26 +0100

    Particle.htmlname: all greek letters nicely show as unicode characters

--- tests/particle/test_particle.py
+++ tests/particle/test_particle.py
@@ -186,25 +186,25 @@ J (total angular) = 1.0    C (charge parity) = -      P (space parity) = -
 
 checklist_htmlname = (
     (1, 'd'),                            # d quark
-    (-2, '~u'),                          # u antiquark
+    (-2, '<SPAN STYLE="text-decoration:overline">u</SPAN>'),  # u antiquark
     (11, 'e<SUP>-</SUP>'),               # e-
-    (-13, '\\mu<SUP>+</SUP>'),           # mu+
-    (-14, '~\\nu<SUB>\\mu</SUB>'),       # nu_mu_bar
+    (-13, 'μ<SUP>+</SUP>'),              # mu+
+    (-14, '<SPAN STYLE="text-decoration:overline">ν</SPAN><SUB>μ</SUB>'),  # nu_mu_bar
     (111, 'π<SUP>0</SUP>'),              # pi0
     (-211, 'π<SUP>-</SUP>'),             # pi-
     (-213, 'ρ(770)<SUP>-</SUP>'),        # rho(770)-
-    (443, 'J/\\psi(1S)'),                # J/psi
-    (300553, '\\Upsilon(4S)'),           # Upsilon(4S)
+    (443, 'J/ψ(1S)'),                    # J/psi
+    (300553, 'Υ(4S)'),                   # Upsilon(4S)
     (321, 'K<SUP>+</SUP>'),              # K+
     (130, 'K<SUB>L</SUB><SUP>0</SUP>'),  # K_L
-    (10321, 'K<SUB>0</SUB><SUP>*</SUP>(1430)<SUP>+</SUP>'),    # K(0)*(1430)+
-    (-10321, '~K<SUB>0</SUB><SUP>*</SUP>(1430)<SUP>-</SUP>'),  # K(0)*(1430)~-
+    (10321, 'K<SUB>0</SUB><SUP>*</SUP>(1430)<SUP>+</SUP>'), # K(0)*(1430)+
+    (-10321, '<SPAN STYLE="text-decoration:overline">K</SPAN><SUB>0</SUB><SUP>*</SUP>(1430)<SUP>-</SUP>'),  # K_bar(0)*(1430)~-
     (2212, 'p'),                         # proton
-    (-2112, '~n'),                       # antineutron
-    (3322, '\\Xi<SUP>0</SUP>'),          # Xi0
-    (-3322, '~\\Xi<SUP>0</SUP>'),        # Xi0_bar
-    (-511, '~B<SUP>0</SUP>'),            # B0_bar
-    (-5122, '~\\Lambda<SUB>b</SUB><SUP>0</SUP>'),  # Lb0_bar
+    (-2112, '<SPAN STYLE="text-decoration:overline">n</SPAN>'),                          # antineutron
+    (3322, 'Ξ<SUP>0</SUP>'),             # Xi0
+    (-3322, '<SPAN STYLE="text-decoration:overline">Ξ</SPAN><SUP>0</SUP>'),              # Xi0_bar
+    (-511, '<SPAN STYLE="text-decoration:overline">B</SPAN><SUP>0</SUP>'),               # B0_bar
+    (-5122, '<SPAN STYLE="text-decoration:overline">Λ</SPAN><SUB>b</SUB><SUP>0</SUP>'),  # Lb0_bar
 )
 
 
