ef4481962a2fef97d06091e171769c295716d5a0
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2019-06-14 10:04:19 +0200

    Release version 0.5.0 (#145)

--- particle/_version.py
+++ particle/_version.py
@@ -3,7 +3,7 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/particle for details.
 
-__version__ = '0.4.4'
+__version__ = '0.5.0'
 
 version = __version__
 version_info = __version__.split('.')
