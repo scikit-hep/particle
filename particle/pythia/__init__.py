216f9094923e6d2012353a040ba43d7a4fc8f1de
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-08-22 00:33:17 +0200

    Introduction of PythiaID class

---
+++ particle/pythia/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2018-2019, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+from __future__ import absolute_import
+
+from .pythiaid import PythiaID
