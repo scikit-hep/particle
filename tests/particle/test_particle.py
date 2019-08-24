93d251aad708588f3d45f0a66628dd4d9844730a
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-08-24 14:52:42 +0200

    Rather use HTML code for Greek letters in LaTeX to HTML conversions

--- tests/particle/test_particle.py
+++ tests/particle/test_particle.py
@@ -250,15 +250,15 @@ def test_explicit_table_loading():
 
 
 checklist_html_name = (
-    (22, 'γ'),                           # photon
+    (22, '&gamma;'),                     # photon
     (1, 'd'),                            # d quark
     (-2, 'u&#773;'),                     # u antiquark
     (11, 'e<SUP>-</SUP>'),               # e-
-    (-13, 'μ<SUP>+</SUP>'),              # mu+
-    (-14, 'ν&#773;<SUB>μ</SUB>'),        # nu_mu_bar
-    (111, 'π<SUP>0</SUP>'),              # pi0
-    (-211, 'π<SUP>-</SUP>'),             # pi-
-    (-213, 'ρ(770)<SUP>-</SUP>'),        # rho(770)-
+    (-13, '&mu;<SUP>+</SUP>'),           # mu+
+    (-14, '&nu;&#773;<SUB>&mu;</SUB>'),  # nu_mu_bar
+    (111, '&pi;<SUP>0</SUP>'),           # pi0
+    (-211, '&pi;<SUP>-</SUP>'),          # pi-
+    (-213, '&rho;(770)<SUP>-</SUP>'),    # rho(770)-
     (20213, 'a<SUB>1</SUB>(1260)<SUP>+</SUP>'),# a_1(1260)+
     (321, 'K<SUP>+</SUP>'),              # K+
     (130, 'K<SUB>L</SUB><SUP>0</SUP>'),  # K_L
@@ -266,15 +266,15 @@ checklist_html_name = (
     (-10321, 'K<SUB>0</SUB><SUP>*</SUP>(1430)<SUP>-</SUP>'),  # K(0)*(1430)-
     (10433, 'D<SUB>s1</SUB>(2536)<SUP>+</SUP>'),              # D_s1(2536)+
     (-511, 'B&#773;<SUP>0</SUP>'),       # B0_bar
-    (443, 'J/ψ(1S)'),                    # J/psi
-    (10441, 'χ<SUB>c0</SUB>(1P)'),       # chi_c0(1P)
-    (300553, 'Υ(4S)'),                   # Upsilon(4S)
+    (443, 'J/&psi;(1S)'),                # J/psi
+    (10441, '&chi;<SUB>c0</SUB>(1P)'),   # chi_c0(1P)
+    (300553, '&Upsilon;(4S)'),           # Upsilon(4S)
     (2212, 'p'),                         # proton
     (-2112, 'n&#773;'),                  # antineutron
-    (-2224, 'Δ&#773;(1232)<SUP>--</SUP>'),   # Delta_bar(1232)--
-    (3322, 'Ξ<SUP>0</SUP>'),             # Xi0
-    (-3322, 'Ξ&#773;<SUP>0</SUP>'),     # Xi0_bar
-    (-5122, 'Λ&#773;<SUB>b</SUB><SUP>0</SUP>')   # Lb0_bar
+    (-2224, '&Delta;&#773;(1232)<SUP>--</SUP>'),   # Delta_bar(1232)--
+    (3322, '&Xi;<SUP>0</SUP>'),             # Xi0
+    (-3322, '&Xi;&#773;<SUP>0</SUP>'),     # Xi0_bar
+    (-5122, '&Lambda;&#773;<SUB>b</SUB><SUP>0</SUP>')   # Lb0_bar
 )
 
 
