1c44888d4266719f3a12659e7a84e5cf40da4155
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-09-06 12:42:44 +0200

    Add tests for PythiaID class

---
+++ tests/pythia/test_pythiaid.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2018-2019, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+import pytest
+
+from particle.pythia import PythiaID
+from particle.pdgid import PDGID
+from particle.exceptions import MatchingIDNotFound
+
+
+def test_class_string_representations():
+    pid = PythiaID(211)
+    assert pid == 211
+    assert pid.__str__() == '<PythiaID: 211>'
+
+
+def test_class_return_type():
+    assert isinstance(-PythiaID(211), PythiaID)
+    assert isinstance(~PythiaID(211), PythiaID)
+
+
+def test_class_inversion():
+    assert -PythiaID(311) == ~PythiaID(311)
+
+
+def test_from_pdgid():
+    assert PythiaID.from_pdgid(9010221) == 10221
+
+    assert PythiaID.from_pdgid(PDGID(9010221)) == 10221
+    assert PythiaID.from_pdgid(PDGID(9010221)) == PythiaID(10221)
+
+    with pytest.raises(MatchingIDNotFound):
+        pdgid = PythiaID.from_pdgid(9000221)
+
+
+def test_to_pdgid():
+    pythiaid = PythiaID(10331)
+    assert pythiaid.to_pdgid() == 10221
+    assert pythiaid.to_pdgid() == PDGID(10221)
