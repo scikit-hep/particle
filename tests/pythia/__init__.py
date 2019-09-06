3bfeee6a2b030496174be184f2001c417b568d09
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2019-09-06 14:06:30 +0200

    Add tests for PythiaID class (#165)

---
+++ tests/pythia/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2018-2019, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
