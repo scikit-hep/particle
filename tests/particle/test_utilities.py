4cbf8dc41d591976290cc6e4d85b24e8f0339ee9
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2020-01-06 16:20:09 +0100

    Update copyright statements to 2020

--- tests/particle/test_utilities.py
+++ tests/particle/test_utilities.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018-2019, Eduardo Rodrigues and Henry Schreiner.
+# Copyright (c) 2018-2020, Eduardo Rodrigues and Henry Schreiner.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/particle for details.
