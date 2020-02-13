e5d435aeba3c189ac1915935273ab0d09c15f9c4
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2020-02-14 00:13:31 +0100

    Bump to 0.9.2

--- particle/_version.py
+++ particle/_version.py
@@ -3,7 +3,7 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/particle for details.
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 version = __version__
 version_info = __version__.split(".")
