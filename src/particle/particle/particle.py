5ea63e20e76b869eb942f7c388f704bcc4d4da9a
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-06-13 00:44:13 +0200

    Try to please MyPy

--- src/particle/particle/particle.py
+++ src/particle/particle/particle.py
@@ -428,7 +428,7 @@ class Particle(object):
 
     @classmethod
     def to_dict(cls, *args, **kwargs):
-        # type: (Any, Any) -> Dict[Iterable[Union[bool, int, str, float]], Iterator[Tuple[Any, ...]]]
+        # type: (Any, Any) -> Dict[Iterable[Iterable[Union[bool, int, str, float]]], Iterable[Iterator[Tuple[Any, ...]]]]
         """
         Render a search (via `findall`) on the internal particle data CSV table
         as a `dict`, loading the table from the default location if no table has yet been loaded.
