scikit-hep/particle

commit 4d58415cc3583192cf2e8df2a3881259fcbd806d

fix: type incorrect for return (#279)

src/particle/particle/particle.py
@@ -288,7 +288,7 @@ class Particle(object):
         particle=None,  # type: Optional[bool]
         **search_terms  # type: Any
     ):
-        # type: (...) -> List[List[Any]]
+        # type: (...) -> List[List[Union[bool, int, str, float]]]
         """
         Render a search (via `findall`) on the internal particle data CSV table
         as a `list`, loading the table from the default location if no table has yet been loaded.
@@ -428,7 +428,7 @@ class Particle(object):
 
     @classmethod
     def to_dict(cls, *args, **kwargs):
-        # type: (Any, Any) -> Dict[List[str], List[Any]]
+        # type: (Any, Any) -> Dict[str, List[Union[str, float, bool, int]]]
         """
         Render a search (via `findall`) on the internal particle data CSV table
         as a `dict`, loading the table from the default location if no table has yet been loaded.
@@ -534,9 +534,10 @@ class Particle(object):
         """
         query_as_list = cls.to_list(*args, **kwargs)
 
-        return dict(
-            zip(query_as_list[0], zip(*query_as_list[1:]))
-        )  # dict(zip(keys, values))
+        keys = query_as_list[0]
+        values = query_as_list[1:]
+
+        return {str(key): value for key, value in zip(keys, values)}
 
     @classmethod
     def load_table(cls, filename=None, append=False, _name=None):

commit 9dc94039e1106aa18a69bf739af64d2e651dcb88

Bug fix for bug introduced in #279 (#305)
* bug fix in Particle.to_dict

* Add a test

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Try to please MyPy

* Try to please MyPy

* fix: mypy now happy, better types

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Henry Schreiner <henryschreineriii@gmail.com>

src/particle/particle/particle.py
@@ -23,6 +23,7 @@ from typing import (
     List,
     Callable,
     Iterable,
+    Iterator,
     SupportsInt,
     Union,
     TextIO,
@@ -427,7 +428,7 @@ class Particle(object):
 
     @classmethod
     def to_dict(cls, *args, **kwargs):
-        # type: (Any, Any) -> Dict[str, List[Union[str, float, bool, int]]]
+        # type: (Any, Any) -> Dict[str, Tuple[Union[bool, int, str, float], ...]]
         """
         Render a search (via `findall`) on the internal particle data CSV table
         as a `dict`, loading the table from the default location if no table has yet been loaded.
@@ -532,11 +533,10 @@ class Particle(object):
         ...    print(tabulate(query_as_dict, headers='keys', tablefmt="rst", floatfmt=".12g", numalign="decimal"), file=outfile)    # doctest: +SKIP
         """
         query_as_list = cls.to_list(*args, **kwargs)
+        headers = query_as_list[0]
+        rows = zip(*query_as_list[1:])
 
-        keys = query_as_list[0]
-        values = query_as_list[1:]
-
-        return {str(key): value for key, value in zip(keys, values)}
+        return {str(h): r for h, r in zip(headers, rows)}
 
     @classmethod
     def load_table(cls, filename=None, append=False, _name=None):

tests/particle/test_particle.py
@@ -603,6 +603,16 @@ def test_to_list():
     assert ["D(s)*+"] in tbl
 
 
+def test_to_dict():
+    query_as_dict = Particle.to_dict(
+        filter_fn=lambda p: p.pdgid.is_lepton and p.charge != 0,
+        exclusive_fields=["name", "charge"],
+        particle=False,
+    )
+
+    assert set(query_as_dict["name"]) == {"e+", "mu+", "tau+", "tau'+"}
+
+
 ampgen_style_names = (
     ("b", 5),
     ("b~", -5),