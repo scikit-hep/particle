88dd216ef2ceed5abbb79f0a667dccbb272aad1c
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-08-18 11:14:07 +0200

    Add a test for duplicates search in .mcd files

---
+++ tests/particle/test_convert.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2021, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+try:
+    from pathlib2 import Path
+except ImportError:
+    from pathlib import Path
+
+import pytest
+
+from particle import data
+from particle.particle.convert import get_from_pdg_mcd
+
+
+DIR = Path(__file__).parent.resolve()
+
+
+def test_get_from_pdg_mcd():
+    with (DIR / "../data/test_PDG_mcd_file_duplicates.mcd").open() as f:
+        with pytest.raises(AssertionError):
+            mcd_table = get_from_pdg_mcd(f)
