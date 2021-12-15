96ab55e3405d8e622363bc06a00cfca89f34f759
Henry Schreiner <henryschreineriii@gmail.com>
2021-12-15 14:03:40 -0500

    fix: recent removals

--- admin/dump_pdgid_to_lhcb.py
+++ admin/dump_pdgid_to_lhcb.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # Copyright (c) 2018-2021, Eduardo Rodrigues and Henry Schreiner.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
