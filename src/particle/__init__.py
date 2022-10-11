cada05cb8317113b4f73b923c111878e50a2aa7d
Ludwig Neste <31670556+The-Ludwig@users.noreply.github.com>
2022-10-11 20:23:21 +0200

    changed order of imports
    More structured order

--- src/particle/__init__.py
+++ src/particle/__init__.py
@@ -8,9 +8,6 @@ from __future__ import annotations
 
 import sys
 
-from .corsika import Corsika7ID
-from .geant import Geant3ID
-
 # Direct access to Particle literals
 # Direct access to Particle (the CSV file is not read until a particle is accessed)
 from .particle import InvalidParticle, Particle, ParticleNotFound, literals
@@ -19,6 +16,8 @@ from .particle.enums import Charge, Inv, Parity, SpinType, Status
 # Direct access to PDGID and other ID classes
 from .pdgid import PDGID
 from .pythia import PythiaID
+from .corsika import Corsika7ID
+from .geant import Geant3ID
 
 # Convenient access to the version number
 from .version import version as __version__
