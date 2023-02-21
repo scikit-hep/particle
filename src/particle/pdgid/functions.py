5bf7cda15022ccb97179acb06f949c45add32858
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2023-02-21 12:42:55 +0100

    Update functions.py

--- src/particle/pdgid/functions.py
+++ src/particle/pdgid/functions.py
@@ -694,7 +694,7 @@ def three_charge(pdgid: PDGID_TYPE) -> int | None:
 
     if charge is not None and int(pdgid) < 0:
         charge = -charge
-    return charge
+    return charge  # noqa: RET504
 
 
 def j_spin(pdgid: PDGID_TYPE) -> int | None:
