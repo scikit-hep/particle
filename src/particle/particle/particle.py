52885a6328f09d05d1929ed1e705a6a38716dff0
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2025-09-30 14:35:54 +0200

    Make particle2025.csv default

--- src/particle/particle/particle.py
+++ src/particle/particle/particle.py
@@ -566,8 +566,8 @@ class Particle:
         assert cls._table_names is not None
 
         if filename is None:
-            with data.basepath.joinpath("particle2024.csv").open() as fa:
-                cls.load_table(fa, append=append, _name="particle2024.csv")
+            with data.basepath.joinpath("particle2025.csv").open() as fa:
+                cls.load_table(fa, append=append, _name="particle2025.csv")
             with data.basepath.joinpath("nuclei2022.csv").open() as fb:
                 cls.load_table(fb, append=True, _name="nuclei2022.csv")
             return
